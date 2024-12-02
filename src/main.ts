fbcfd852be8e0c459eaad3979386f9cbb9819d15
Gurkan <hello@gurkz.me>
2024-12-02 13:09:27 +0100

    setup biome

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
 });
 
 client.once("ready", () => {
-  console.log("ready");
+	console.log("ready");
 });
 
 client.login(Bun.env.TOKEN);
